751aaa6450f1c8c5650a708765ab64d692bbadd7
Klutzz <15657303025@163.com>
2019-11-15 22:04:52 +0800

    修改UserDao 新增了根据姓名查询用户方法

--- src/main/java/com/offcn/dao/UserDao.java
+++ src/main/java/com/offcn/dao/UserDao.java
@@ -4,4 +4,8 @@ import com.offcn.po.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDao extends JpaRepository<User, Long> {
+
+    // 新增根据名字查询
+    public User findByNameIs();
+
 }
